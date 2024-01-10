6027038ecf8fad6f8daff661b34c4af9544f3e2d
Antonio_Chacon <antonio_chacon@epam.com>
2024-01-10 17:18:35 +0100

    Migrating to Spring Boot

--- src/main/java/com/epam/esm/repository/CertificatesRepository.java
+++ src/main/java/com/epam/esm/repository/CertificatesRepository.java
@@ -1,13 +1,9 @@
 package com.epam.esm.repository;
 
 import com.epam.esm.model.GiftCertificate;
-import com.epam.esm.model.Tag;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,11 +11,4 @@ public interface CertificatesRepository extends JpaRepository<GiftCertificate, L
     JpaSpecificationExecutor<GiftCertificate> {
 
   Optional<GiftCertificate> findCertificateByName(String certificateName);
-
-  String GET_TAGS_BY_CERTIFICATE_ID = "SELECT t.* FROM tag t INNER JOIN " +
-      "gift_certificate_tag gct ON t.tag_id = gct.tag_id " +
-      "WHERE gct.certificate_id = :giftCertificateId";
-
-  @Query(value = GET_TAGS_BY_CERTIFICATE_ID, nativeQuery = true)
-  List<Tag> findTagsByCertificateId(@Param("giftCertificateId") Long giftCertificateId);
 }
